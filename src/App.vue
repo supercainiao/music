4f40d4b54adb15c1979b09fad6abe3f1215d6f90
supercainiao <1253157617@qq.com>
2018-04-05 18:36:36 +0800

    s

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <m-headr></m-headr>
+    <tab></tab>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+ import MHeadr from './components/m-header/m-header'
+ import Tab from './components/tab/tab'
+  export default {
+    components: {
+      MHeadr,
+      Tab
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+    @import "./common/stylus/base";
+    @import "./common/stylus/variable";
+    #app
+      color: $color-text
+</style>
---
+++ src/common/js/jsonp.js
@@ -0,0 +1,21 @@
+import originJSONP from 'jsonp'
+
+export default function jsonp(url, data, option) {
+    return new Promise((resolve, reject) => {
+        url += (url.indexOf('?') < 0 ? '?' : '&') + param(data);
+        originJSONP(url, option, (err, data) => {
+            if(!err)
+                resolve(data);
+            else
+                reject(err);
+        })
+    })
+}
+function param(data) {
+    let url = '';
+    for(var key in data){
+        let value = data[key] !== undefined ? data[key] : '';
+        url += `&${key}=${encodeURIComponent(value)}`;
+    }
+    return url ? url.substring(1) : '';
+}

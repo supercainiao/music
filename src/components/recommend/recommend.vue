6c00121286664009ba238a139c4995c39da244f0
supercainiao <1253157617@qq.com>
2018-04-07 02:47:07 +0800

    懒加载

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,36 +1,51 @@
 <template>
   <div class="recommend">
-      <div class="recommend-content">
-          <div v-if="recommends.length" class="slider-wraper">
-              <slider>
-                  <div v-for="(item,ind) in recommends" :key="ind" slot="sliderImg">
-                      <img :src="item.picUrl" />
-                  </div>
-              </slider>
-          </div>
-          <div class="recommend-list">
-              <h1 class="list-title">热门歌单推荐</h1>
-              <ul>
-
-              </ul>
-          </div>
-      </div>
+      <scroll ref="scroll" class="recommend-content" :data="discList">
+            <div>
+                <div v-if="recommends.length" class="slider-wraper">
+                    <slider>
+                        <div v-for="(item,ind) in recommends" :key="ind" slot="sliderImg">
+                            <a :href="item.linkUrl">
+                                <img class="needsclick" @load="loadImg" :src="item.picUrl" />
+                            </a>
+                        </div>
+                    </slider>
+                </div>
+                <div class="recommend-list">
+                    <h1 class="list-title">热门歌单推荐</h1>
+                    <ul>
+                        <li v-for="(item, index) in discList" :key="index" class="item">
+                            <div class="icon">
+                                <img v-lazy="item.imgurl" :alt="item.dissname" style="width:60px;height:60px;"/>
+                            </div>
+                            <div class="text">
+                                <h2 class="name">{{item.creator.name}}</h2>
+                                <p class="desc">{{item.dissname}}</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+      </scroll>
   </div>
 </template>
 
 <script>
-    import getRecommend from '../../api/recommend.js'
+    import {getRecommend,getDiscList} from '../../api/recommend.js'
     import {ERR_OK} from '../../api/config.js'
     import Slider from '@/base/slider/slider.vue'
+    import Scroll from '@/base/scroll/scroll.vue'
 
     export default {
         data() {
             return {
-                recommends:[]
+                recommends:[],
+                discList:[]
             }
         },
         created() {
             this._getRecommend();
+            this._getDiscList();
         },
         methods:{
             _getRecommend() {
@@ -41,60 +56,73 @@
                         this.recommends = res.data.slider;
                     }   
                 })
+            },
+            _getDiscList() {
+                getDiscList().then((res) => {
+                    this.discList = res.data.list;
+                })
+            },
+            loadImg() {
+                if(!this.isloaded)
+                {
+                    this.$refs.scroll.refresh();
+                    this.isloaded = true;
+                }
             }
         },
         components:{
-            Slider:Slider
+            Slider:Slider,
+            Scroll:Scroll
         }
     }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-    // @import '../../common/stylus/variable'
-    // .recommend
-    // position: fixed
-    // width: 100%
-    // top: 88px
-    // bottom: 0
-    // .recommend-content
-    //   height: 100%
-    //   overflow: hidden
-    //   .slider-wrapper
-    //     position: relative
-    //     width: 100%
-    //     overflow: hidden
-    //   .recommend-list
-    //     .list-title
-    //       height: 65px
-    //       line-height: 65px
-    //       text-align: center
-    //       font-size: $font-size-medium
-    //       color: $color-theme
-    //     .item
-    //       display: flex
-    //       box-sizing: border-box
-    //       align-items: center
-    //       padding: 0 20px 20px 20px
-    //       .icon
-    //         flex: 0 0 60px
-    //         width: 60px
-    //         padding-right: 20px
-    //       .text
-    //         display: flex
-    //         flex-direction: column
-    //         justify-content: center
-    //         flex: 1
-    //         line-height: 20px
-    //         overflow: hidden
-    //         font-size: $font-size-medium
-    //         .name
-    //           margin-bottom: 10px
-    //           color: $color-text
-    //         .desc
-    //           color: $color-text-d
-    //   .loading-container
-    //     position: absolute
-    //     width: 100%
-    //     top: 50%
-    //     transform: translateY(-50%)                    
+    @import '../../common/stylus/variable'
+   .recommend
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .recommend-content
+      height: 100%
+      overflow: hidden
+      .slider-wrapper
+        position: relative
+        width: 100%
+        overflow: hidden
+      .recommend-list
+        .list-title
+          height: 65px
+          line-height: 65px
+          text-align: center
+          font-size: $font-size-medium
+          color: $color-theme
+        .item
+          display: flex
+          box-sizing: border-box
+          align-items: center
+          padding: 0 20px 20px 20px
+          .icon
+            flex: 0 0 60px
+            width: 60px
+            padding-right: 20px
+          .text
+            display: flex
+            flex-direction: column
+            justify-content: center
+            flex: 1
+            line-height: 20px
+            overflow: hidden
+            font-size: $font-size-medium
+            .name
+              margin-bottom: 10px
+              color: $color-text
+            .desc
+              color: $color-text-d
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)       
 </style>
--- src/main.js
+++ src/main.js
@@ -6,10 +6,14 @@ import App from './App'
 import router from './router'
 import './common/stylus/index.styl'
 import fastclick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.config.productionTip = false
 
 fastclick.attach(document.body);
+Vue.use(VueLazyLoad,{
+  loading: require('./common/image/default.png')
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',

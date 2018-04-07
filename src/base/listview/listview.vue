039dc0a9cda6258f48000a51dd78746c654d8ff6
supercainiao <1253157617@qq.com>
2018-04-07 23:44:28 +0800

    [右侧滑动]

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -1,7 +1,7 @@
 <template>
-    <scroll class="listview" :data="data">
+    <scroll class="listview" :data="data" ref="listview">
         <ul>
-            <li v-for="(group,index) in data" :key="index" class="list-group">
+            <li v-for="(group,index) in data" :key="index" class="list-group" ref="listGroup">
                 <h2 class="list-group-title">{{ group.title }}</h2>
                 <ul>
                     <li v-for="(item,ind) in group.items" :key="ind" class="list-group-item">
@@ -11,16 +11,55 @@
                 </ul>
             </li>
         </ul>
+        <div class="list-shortcut" @touchstart="onShortcutTouchStart" @touchmove.stop.prevent="onShortcutTouchMove">
+            <ul>
+                <li v-for="(item, ind) in shortcutList" :key="ind" :data-index="ind" class="item">
+                    {{ item }}
+                </li>
+            </ul>
+        </div>
     </scroll>
 </template>
 
 <script>
 import Scroll from '../scroll/scroll.vue'
+import $ from 'jquery'
+
+const ANCHOR_HEIGHT = 18 //右边abcd每项的高度18px
 export default {
   props: {
       data:{
           type:Array,
           default:[]
+      }
+  },
+  created() {
+      this.touch = {}
+  },
+  computed:{
+      shortcutList() {
+          return this.data.map((item) => {
+              return item.title.substring(0,1);
+          })
+      }
+  },
+  methods:{
+      onShortcutTouchStart(e) {
+          let anchorIndex = $(e.target).attr('data-index');
+          this.touch.anchorIndex = anchorIndex;
+          let firstTouch = e.touches[0];
+          this.touch.y1 = firstTouch.pageY;
+          this._scrollTo(anchorIndex);
+      },
+      onShortcutTouchMove(e) {
+          let firstTouch = e.touches[0];
+          this.touch.y2 = firstTouch.pageY;
+          let delta = (this.touch.y2 - this.touch.y1) / ANCHOR_HEIGHT | 0;//计算y轴滑动的多少项，'|0'(math.floor)就是向下取整
+          let anchorIndex = parseInt(this.touch.anchorIndex) + delta;
+          this._scrollTo(anchorIndex);
+      },
+      _scrollTo(index) {
+          this.$refs.listview.scrollToElement(this.$refs.listGroup[index], 0);//第一个参数为要滚动到的元素，第二个参数为滚动延时时间
       }
   },
   components: {
--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -10,3 +10,7 @@ export function hasClass(el, className) {
     let reg = new RegExp('(^|\\s)'+className+'(\\s|$)');
     return reg.test(el.className);
 }
+
+// export function getData(el, name, val) {
+
+// }
